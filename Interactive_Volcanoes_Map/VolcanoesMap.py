353cf56d14a4496a4c95bf1ed4dd36a025db48eb
RobertoMartinR <robertomartinromoo@gmail.com>
2025-01-31 21:16:21 +0100

    Write the explanation of the project

--- Interactive_Volcanoes_Map/VolcanoesMap.py
+++ Interactive_Volcanoes_Map/VolcanoesMap.py
@@ -1,12 +1,21 @@
+""" 
+This is a project I made following the Python Mega Course in Udemy, the purpose is to create an interactive map of whatever you want. 
+In this case the map is about Volcanoes and where they are in the United States of America, each volcanoe has a pop up feature to see different information of it.
+All the information in this case is stored in a txt file.
+"""
+
 import folium 
 import pandas as pd
 
+# Read the txt file
 data = pd.read_csv('Volcanoes.txt')
 
+# Get the latitude, longitude and elevation values
 latitude = list(data['LAT'])
 longitude = list(data['LON'])
 elev = list(data['ELEV'])
 
+# Define the icons colours depending on the elevation
 def icon_color(elevation):
     if elevation < 1000:
         return 'green'
@@ -15,14 +24,17 @@ def icon_color(elevation):
     else: 
         return 'red'
 
+# Create the map first location, this is where we will be located when he run the script and the map it's created
 map = folium.Map(location=[38.58, -99.09], zoom_start=5, tiles='OpenStreetMap')
 
+# Create the Group Volcanoes
 fgv = folium.FeatureGroup(name='Volcanoes')
 
+# Create a Circle marker for each Volcanoe in the txt file based on the latitude and longitude
 for lt, ln, el in zip(latitude, longitude, elev):
     fgv.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=folium.Popup(str(el) + ' m', parse_html=True), fill_color=icon_color(el)
                                      , color='grey', fill_opacity=0.7))
-    
+
 fgp = folium.FeatureGroup(name='Population')
 
 fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(), 
